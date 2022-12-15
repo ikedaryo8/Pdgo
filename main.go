091fe56e80a249d6867d20ab6b6cf8572403498b
Ryoya Ikeda <ikedaryo8@gmail.com>
2022-12-15 20:18:47 +0900

    一連の流れを実装

--- main.go
+++ main.go
@@ -6,14 +6,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// type FormData struct {
-// 	UserName    string `from : "UserName"`
-// 	MailAddress string `from : "MailAddress" `
-// 	Age         string `from : "Age"`
-// 	Gender      string `from : "Gender"`
-// 	OnBusstop   string `from : "OnBusstop"`
-// 	OffBusstop  string `from : "OffBusstop"`
-// }
+type FormData struct {
+	UserName    string
+	MailAddress string
+	Age         string
+	Gender      string
+	OnBusstop   string
+	OffBusstop  string
+	isNeedHelp  string
+}
 
 func main() {
 	router := gin.Default()
@@ -27,17 +28,8 @@ func main() {
 	router.GET("/reservation", getForm)
 	// フォーム送信
 	router.POST("/reservation", postForm)
-	// router.POST("/sendform", func(c *gin.Context) {
-	// 	UserName := c.PostForm("User_name")
-	// 	UserMailAddress := c.PostForm("mail")
-	// 	UserAge := c.PostForm("age")
-	// 	UserGender := c.PostForm("gender")
-	// 	BusstopON := c.PostForm("board_bus_stop")
-	// 	BusstopOFf := c.PostForm("get_off_bus_stop")
-	// 	c.String(http.StatusOK)
-
-	// })
 	router.Run(":8080")
+
 }
 
 func test(ctx *gin.Context) {
@@ -52,21 +44,31 @@ func getForm(ctx *gin.Context) {
 	ctx.HTML(200, "form.tmpl", gin.H{})
 }
 func postForm(ctx *gin.Context) {
-	//UserName := ctx.PostForm("User_name")
-	ctx.JSON(http.StatusOK, gin.H{
-		"name":       ctx.PostForm("User_name"),
-		"mail":       ctx.PostForm("mail"),
-		"age":        ctx.PostForm("age"),
-		"gender":     ctx.PostForm("gender"),
-		"onBusstop":  ctx.PostForm("board_bus_stop"),
-		"offBusstop": ctx.PostForm("get_off_bus_stop"),
-		"isNeedHelp": ctx.PostForm("isNeedHelp"),
-	})
-	checkAnser()
+
+	var data FormData
+	data.UserName = ctx.PostForm("User_name")
+	data.MailAddress = ctx.PostForm("mail")
+	data.Age = ctx.PostForm("age")
+	data.Gender = ctx.PostForm("gender")
+	data.OnBusstop = ctx.PostForm("board_bus_stop")
+	data.OffBusstop = ctx.PostForm("get_off_bus_stop")
+	data.isNeedHelp = ctx.PostForm("isNeedHelp")
+
+	// 乗降バス停の重複を弾く
+	if checkAnser(data.OnBusstop, data.OffBusstop) {
+		ctx.HTML(200, "form.tmpl", gin.H{})
+
+	} else {
+		ctx.HTML(200, "result.tmpl",
+			gin.H{
+				"User_name":        data.UserName,
+				"board_bus_stop":   data.OnBusstop,
+				"get_off_bus_stop": data.OffBusstop,
+				"isNeedHelp":       data.isNeedHelp,
+			})
+	}
 }
 
-func checkAnser() {
-	// 空の値はないか
-	// バス停の重複はないか
-	//
+func checkAnser(on string, off string) bool {
+	return on == off
 }
