a61432317aaa481e3b4600302c26c958f9aeb3f0
Ryoya Ikeda <ikedaryo8@gmail.com>
2022-12-15 12:02:28 +0900

    get できるようになった

--- main.go
+++ main.go
@@ -20,26 +20,32 @@ import (
 func main() {
 	router := gin.Default()
 	router.Static("/static", "./static") //" URL", 格納場所
-	router.LoadHTMLGlob("static/*")
+	router.LoadHTMLGlob("./static/*.tmpl")
 
-	router.GET("/dev", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
+	router.GET("/dev", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
 			"message": "hello world",
 		})
 	})
 	//
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(200, "static/index.html", gin.H{})
+		ctx.HTML(200, "index.tmpl", gin.H{})
+	})
+	router.GET("/reservation", func(ctx *gin.Context) {
+		ctx.HTML(200, "form.tmpl", gin.H{})
+
 	})
 
-	router.POST("/PostForm", func(c *gin.Context) {
-		UserName := c.PostForm("User_name")
-		UserMailAddress := c.PostForm("mail")
-		UserAge := c.PostForm("age")
-		UserGender := c.PostForm("gender")
-		bus
-		c.String(http.StatusOK, message)
-	})
+	// router.POST("/send", func(c *gin.Context) {
+	// 	UserName := c.PostForm("User_name")
+	// 	UserMailAddress := c.PostForm("mail")
+	// 	UserAge := c.PostForm("age")
+	// 	UserGender := c.PostForm("gender")
+	// 	BusstopON := c.PostForm("board_bus_stop")
+	// 	BusstopOFf := c.PostForm("get_off_bus_stop")
+	// 	c.String(http.StatusOK)
+
+	// })
 	router.Run(":8080")
 
 }
